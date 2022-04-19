0a3cf5dad072fbc86bdc5bb21e37d087607e4547
shecho <shechovala@gmail.com>
2022-04-18 21:44:08 -0500

    added create from

--- client/src/pages/todos/create.tsx
+++ client/src/pages/todos/create.tsx
@@ -1,6 +1,56 @@
-import { FC } from 'react'
+import { Button, Container, FormControl, FormLabel, Icon, Input, Link as ChakraLink } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import { FC, useState } from 'react'
+import { FaTimes } from 'react-icons/fa'
 
+interface Todo {
+  cliente?: string
+  observaciones?: string
+  prioridad?: string
+  stado?: string
+  tarea?: string
+  tiempo?: number
+  updatedAt?: Date
+  _id?: string
+}
 const TodoCreatePage: FC = () => {
-  return <>Create todo</>
+  const [newTodo, setNewTodo] = useState<Partial<Todo>>({})
+
+  const handleInputChange = (e: any) => {
+    setNewTodo((prevState) => ({ ...prevState, [e.target.name]: e.target.value }))
+    console.log(newTodo)
+  }
+  return (
+    <Container>
+      <FormControl>
+        <FormLabel htmlFor="cliente">Cliente</FormLabel>
+        <Input id="cliente" name="cliente" type="text" mb="3" onChange={handleInputChange} />
+
+        <FormLabel htmlFor="prioridad">Prioridad</FormLabel>
+        <Input id="prioridad" name="prioridad" type="text" mb="3" onChange={handleInputChange} />
+
+        <FormLabel htmlFor="estado">Estado</FormLabel>
+        <Input id="estado" name="estado" type="text" mb="3" onChange={handleInputChange} />
+
+        <FormLabel htmlFor="tarea">Tarea</FormLabel>
+        <Input id="tarea" name="tarea" type="text" mb="3" onChange={handleInputChange} />
+
+        <FormLabel htmlFor="tiempo">Tiempo en horas</FormLabel>
+        <Input id="tiempo" name="tiempo" type="number" mb="3" onChange={handleInputChange} />
+
+        <FormLabel htmlFor="observaciones">Observaciones</FormLabel>
+        <Input id="observaciones" name="observaciones" type="text" mb="3" onChange={handleInputChange} />
+
+        <Button type="submit" m="2" isLoading={false} loadingText="Enviando" colorScheme="teal" variant="outline">
+          Enviar
+        </Button>
+        <ChakraLink as={Link as any} to="/todos">
+          <Button leftIcon={<Icon as={FaTimes} />} loadingText="Enviando" colorScheme="red" variant="outline">
+            Cancelar
+          </Button>
+        </ChakraLink>
+      </FormControl>
+    </Container>
+  )
 }
 export default TodoCreatePage
--- client/src/pages/todos/index.tsx
+++ client/src/pages/todos/index.tsx
@@ -1,7 +1,9 @@
-import { Heading, SimpleGrid } from '@chakra-ui/react'
+import { Button, Heading, Icon, Link as ChakraLink, SimpleGrid, Stack } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 import CardItem from '@components/card/card-item'
 import { queryAllTodos } from '@src/shared/queries/todo/query'
 import { FC } from 'react'
+import { FaPlus } from 'react-icons/fa'
 import { useQuery } from 'react-query'
 import { GET_TODOS_LIST } from '../../shared/queries/constans'
 
@@ -13,14 +15,23 @@ const TodoListPage: FC = () => {
 
   return (
     <>
-      {isLoading && <Heading>Loading...</Heading>}
-      {!isLoading && data && data.todos && <Heading> {data.message}</Heading>}
-      {!isLoading && !data.todos && <Heading> No hay tareas disponibles</Heading>}
-      {!isLoading && data && data.todos && (
-        <SimpleGrid height="auto" width="7xl" columns={2} spacingX="40px" spacingY="20px">
-          {data && data?.todos?.map((item: any) => <CardItem todo={item} key={`${item.id}`} />)}
-        </SimpleGrid>
-      )}
+      <Stack flexDirection="row-reverse" direction="row" spacing={4} mb="5">
+        <ChakraLink as={Link as any} to="create">
+          <Button leftIcon={<Icon as={FaPlus} />} colorScheme="teal" variant="solid">
+            New Todo
+          </Button>
+        </ChakraLink>
+      </Stack>
+
+      <Stack direction="row" spacing={2}>
+        {isLoading && <Heading>Loading...</Heading>}
+        {!isLoading && !data.todos && <Heading> No hay tareas disponibles</Heading>}
+        {!isLoading && data && data.todos && (
+          <SimpleGrid height="auto" width="7xl" columns={2} spacingX="40px" spacingY="15px">
+            {data && data?.todos?.map((item: any) => <CardItem todo={item} key={`${item._id}`} />)}
+          </SimpleGrid>
+        )}
+      </Stack>
     </>
   )
 }
