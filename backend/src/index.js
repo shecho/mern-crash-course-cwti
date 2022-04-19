d7df38793864b8c42d5cdd3aa099ac2f4c1a1597
shecho <shechovala@gmail.com>
2022-04-18 20:44:08 -0500

    congif

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,25 @@
 import express from "express";
 
 const app = express();
+
 app.set("port", 5000);
 
 async function main() {
-  await app.listen(app.get("port"));
+  app.listen(app.get("port"));
   console.log(`Connected on port: http://localhost:${app.get("port")}/`);
 
   app.get("/", (req, res) => {
-    console.log("Hello world");
-    // res.json({
-    //   message: "Mi primer servidor con express",
-    // });
+    res.json({
+      message: "Mi primer servidor con express",
+      message2: "Mi primer servidor con express2",
+    });
+  });
+
+  app.get("/users", (req, res) => {
+    res.json({
+      user1: "Willy",
+      user2: "Wilder",
+    });
   });
 }
 
